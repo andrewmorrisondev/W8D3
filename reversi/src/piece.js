5c8d8f6d5db3ff6aca681b12243b78c0322f8ada
Olga <olga.sleepless@gmail.com>
2023-03-01 14:02:54 -0500

    Add Piece

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -2,18 +2,28 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
-}
+    this.color = color;
+};
+
+//const colors = ['white','black'];
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === 'white') {
+        return 'black';
+    } else {
+        return 'white';
+    };
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    this.color === 'white' ? this.color = 'black' : this.color = 'white';
+    
 };
 
 /**
